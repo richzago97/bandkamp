b604b3b07dd7dfc2807afc998834bca4d6708fe2
richzago <rich.czago@gmail.com>
2022-12-14 16:25:39 -0300

    refactor: change the name to call id

--- albums/urls.py
+++ albums/urls.py
@@ -5,5 +5,5 @@ from songs import views as song_views
 
 urlpatterns = [
     path("albums/", views.AlbumView.as_view()),
-    path("albums/<int:pk>/songs/", song_views.SongView.as_view()),
+    path("albums/<int:album_id>/songs/", song_views.SongView.as_view()),
 ]

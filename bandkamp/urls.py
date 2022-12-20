2243947609e60536b45023507be086375c3f29ff
richzago <rich.czago@gmail.com>
2022-12-20 16:18:24 -0300

    fix

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -26,13 +26,10 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path("api/", include("albums.urls")),
     path("api/", include("songs.urls")),
-    path("api/docs/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/docs/swagger-ui/",
+        "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ),
-    path(
-        "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
 ]
